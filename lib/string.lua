cdb9675ffade1b720209377e96731ee78971518f
xiedacon <xiedacon@qq.com>
2018-10-04 23:13:31 +0800

    feat: string

---
+++ lib/string.lua
@@ -0,0 +1,69 @@
+-- Copyright (c) 2018, xiedacon.
+
+local String = {}
+
+function String.replace(s, str1, str2)
+    return table.concat(String.split(s, str1), str2)
+end
+
+function String.slice(s, from, to)
+    local len = #s
+
+    from = from or 1
+    if from < 0 then from = len + from + 1 end
+    if from < 0 then from = 1 end
+
+    to = to or (len + 1)
+    if to < 1 then 
+        to = len + to
+    elseif to > len + 1 then
+        to = len
+    else
+        to = to - 1
+    end
+
+    return string.sub(s, from, to)
+end
+
+function String.split(s, delimiter)
+    local arr = {}
+
+    if delimiter == "" then
+        for i = 1, #s do
+            arr[i] = string.sub(s, i, i)
+        end
+
+        return arr
+    end
+
+    local i = 1
+    local current = 1
+    s = s .. delimiter
+    while(current < #s) do
+        local from, to = string.find(s, delimiter, current)
+        
+        arr[i] = string.sub(s, current, from - 1)
+        current = to + 1
+        i = i + 1
+    end
+
+    return arr
+end
+
+function String.startsWith(s, prefix)
+    return string.sub(s, 1, #prefix) == prefix
+end
+
+function String.trim(s)
+    return s:gsub("^%s*(.-)%s*$", "%1")
+end
+
+function String.trimLeft(s)
+    return s:gsub("^%s*(.-)$", "%1")
+end
+
+function String.trimRight(s)
+    return s:gsub("^(.-)%s*$", "%1")
+end
+
+return String
