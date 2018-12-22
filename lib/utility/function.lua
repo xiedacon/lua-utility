d91340d9a2ff5b0bf537091726efbb8a748e9595
xiedacon <xiedacon@qq.com>
2018-12-22 15:26:34 +0800

    feat: pre build function bodys

--- lib/utility/function.lua
+++ lib/utility/function.lua
@@ -39,14 +39,34 @@ function Function:call(...)
     return Function.apply(fn, args)
 end
 
+local function build_func_body(n)
+    local args = table.new(n, 0)
+
+    for i = 1, n do
+        args[i] = "args[" .. i .. "]"
+    end
+
+    return "return fn(" .. table.concat(args, ", ") .. ")"
+end
+
+local pre_build = 20
+local func_bodys = table.new(pre_build, 0)
+for i = 1, pre_build do
+    func_bodys[i] = build_func_body(i - 1)
+end
+
 function Function:apply(args)
     local fn = self
-    local args = Array(args)
-
-    return load("return fn(" .. Object.keys(args):map(function(i) return "args[" .. i .. "]" end):join(", ") .. ")", nil, nil, {
+    local ok, anonymous = pcall(load, func_bodys[#args + 1] or build_func_body(#args), nil, nil, {
         fn = fn,
         args = args
-    })()
+    })
+
+    if ok then
+        return anonymous()
+    else
+        return nil, anonymous
+    end
 end
 
 return Function
