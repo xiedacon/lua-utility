669ec7053f0317250ad9d9da5bff1e038ef64e87
xiedacon <xiedacon@qq.com>
2025-03-12 23:21:37 +0800

    chore: bump version to 0.4

--- lib/utility.lua
+++ lib/utility.lua
@@ -1,7 +1,7 @@
 -- Copyright (c) 2018, xiedacon.
 
 local utility = {
-    _VERSION = "0.3",
+    _VERSION = "0.4",
     array = require("utility.array"),
     func = require("utility.function"),
     object = require("utility.object"),
